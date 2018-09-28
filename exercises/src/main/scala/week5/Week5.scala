f806df257a3b4cdd95ed732734eb425da48c7a76
Alexander Vivas <falexvr@gmail.com>
2018-09-27 19:40:46 -0500

    Lists operations

--- exercises/src/main/scala/week5/Week5.scala
+++ exercises/src/main/scala/week5/Week5.scala
@@ -31,11 +31,30 @@ object Week5 {
 
   def removeAtV2[T](xs: List[T], n: Int): List[T] = (xs take n) ::: (xs drop n + 1)
 
-  def flatten(xs: List[Any]): List[Any] = xs match {
-    case List() => throw new Error("flatten of empty list")
-    case y :: ys => List(y match {
-      case List(_) => flatten(_)
-      case _ => _
-    }) ++ flatten(ys)
+  def flatten(xs: List[Any]): List[Any] = {
+    def innerFlatten(x: Any): List[Any] = x match {
+      case y: List[Any] => flatten(y)
+      case y => List(y)
+    }
+    xs match {
+      case List() => throw new Error("flatten of empty list")
+      case List(x) => innerFlatten(x)
+      case y :: ys => innerFlatten(y) ++ flatten(ys)
+    }
+  }
+
+  def msort(xs: List[Int]): List[Int] = {
+    val n = xs.length / 2
+    if(n == 0) xs
+    else {
+      def merge(xs: List[Int], ys: List[Int]): List[Int] = (xs, ys) match {
+        case (_, Nil) || (Nil, _) => _
+        case (x :: xs1, y :: ys1) =>
+          if(x < y) x :: merge(xs1, ys)
+          else y :: merge(xs, ys1)
+      }
+      val (fst, snd) = xs splitAt n
+      merge(msort(fst), msort(snd))
+    }
   }
 }
